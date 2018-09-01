e153fffa76da0e4ccf6b50b07b4af742bfff0138
lucaskurata <lucas.kuratahotmail.com>
2018-09-01 10:42:21 -0300

    Criação de um arquivo em .py

---
+++ AC3LP2.py
@@ -0,0 +1,13 @@
+class Registro:
+    def __init__ (self):
+        self.nome="Lucas"
+        self.cpf=50885168871
+        self.rg=559810372
+        self.endereço="av Rudge, 315"
+
+cadastro=Registro()
+
+print(cadastro.nome)
+print(cadastro.cpf)
+print(cadastro.rg)
+print(cadastro.endereço)
