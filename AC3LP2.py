6236f368a04ed3d28ed0840596e19aa6b16e9689
lucaskurata <lucas.kuratahotmail.com>
2018-09-01 10:47:55 -0300

    Ateração do arquivo .py

--- AC3LP2.py
+++ AC3LP2.py
@@ -2,11 +2,17 @@ class Registro:
     def __init__ (self):
         self.nome="Lucas"
         self.cpf=50885168871
-        self.rg=559810372
+        self.rg=50885168871
         self.endereço="av Rudge, 315"
 
-cadastro=Registro()
+    def validacao(self):
+        if self.cpf!=self.rg:
+            return True
+        else:
+            return False
 
+cadastro=Registro()
+print("Validação para Cpf e Rg válidos ou inválidos...",cadastro.validacao())
 print(cadastro.nome)
 print(cadastro.cpf)
 print(cadastro.rg)
